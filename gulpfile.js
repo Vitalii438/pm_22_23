21e6162ae18cf3b19da4a2876e61e82df43f82e9
Vitalii438 <Vitali.Somyk@gmail.com>
2025-10-03 00:47:15 +0300

    Components, Gulp tasks, html includes.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const rename = require("gulp-rename");
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify");
 const htmlmin = require("gulp-htmlmin");
+const fileinclude = require("gulp-file-include");
 const imagemin = require("gulp-imagemin");
 const jsmin = require("gulp-jsmin");
 const browserSync = require("browser-sync").create();
@@ -27,28 +28,47 @@ function reload(done) {
 
 function watchFiles() {
   watch("app/*.html", series(html_task, reload));
+  watch("app/components/html/*.html", series(html_task, reload));
   watch("app/css/*.css", series(css_task, reload));
   watch("app/scss/*.scss", series(scss_task, reload));
+  watch("app/components/scss/*.scss", series(scss_task, reload));
   watch("app/js/*.js", series(js_task, reload));
   watch("app/img/*", series(img_task, reload));
   watch("app/svg/*", series(svg_task, reload));
 }
 
-const build = parallel(html_task, scss_task, css_task, img_task, svg_task, js_task, bootstrapCSS, bootstrapJS);
+const build = parallel(
+  html_task,
+  scss_task,
+  css_task,
+  img_task,
+  svg_task,
+  js_task,
+  bootstrapCSS,
+  bootstrapJS
+);
 exports.default = series(build, serve, watchFiles);
 
 function bootstrapCSS(done) {
-    return src("node_modules/bootstrap/dist/css/bootstrap.min.css")
-        .pipe(dest("dist/css"));
+  return src("node_modules/bootstrap/dist/css/bootstrap.min.css").pipe(
+    dest("dist/css")
+  );
 }
 
 function bootstrapJS(done) {
-    return src("node_modules/bootstrap/dist/js/bootstrap.bundle.min.js")
-        .pipe(dest("dist/js"));
+  return src("node_modules/bootstrap/dist/js/bootstrap.bundle.min.js").pipe(
+    dest("dist/js")
+  );
 }
 
 function html_task(done) {
   return src("app/*.html")
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file",
+      })
+    )
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(
       through.obj(function (file, enc, cb) {
@@ -62,14 +82,14 @@ function html_task(done) {
 }
 
 function css_task(done) {
-  return src("app/css/*.css")
+  return src(["app/*.css", "app/components/*.css"])
     .pipe(cssnano())
     .pipe(rename({ suffix: ".min" }))
     .pipe(dest("dist/css"));
 }
 
 function scss_task(done) {
-  return src("app/scss/*.scss")
+  return src("app/index.scss")
     .pipe(sass())
     .pipe(cssnano())
     .pipe(rename({ suffix: ".min" }))
@@ -77,19 +97,18 @@ function scss_task(done) {
 }
 
 function img_task(done) {
-  return src("app/img/*")
+  return src("app/components/img/*")
     .pipe(imagemin())
     .pipe(rename({ suffix: ".min" }))
     .pipe(dest("dist/img"));
 }
 
 function svg_task(done) {
-    return src("app/svg/*.svg")
-        .pipe(dest("dist/svg"));
+  return src("app/components/svg/*.svg").pipe(dest("dist/svg"));
 }
 
 function js_task(done) {
-  return src("app/js/*.js")
+  return src("app/components/js/*.js")
     .pipe(jsmin())
     .pipe(rename({ suffix: ".min" }))
     .pipe(dest("dist/js"));
